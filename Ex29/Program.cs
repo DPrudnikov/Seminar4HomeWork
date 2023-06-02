7fd9ca2eb4251bbac4add315bf67bb7062e424c8
DPrudnikov <dima1911@mail.ru>
2023-06-02 17:51:29 +0300

    Add Ex29

---
+++ Ex29/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов 
+// и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int [] array = {1,4,6,16,5,5,32,7};
+
+int length = array.Length;
+int count = 0;
+
+while (count < length)
+{
+    Console.WriteLine (array[count]);
+    count++;
+}
+Console.WriteLine ("End");
